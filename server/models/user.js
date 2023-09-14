d7233ad69891cfaf322c8721356a5989b2188a51
Raul Remesal van Merode <raul.remesal@gmail.com>
2023-09-14 08:01:56 +0200

    removed any usage of mysql library

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,3 @@
-import connect from '../db/connect.js'
 import bcrypt from 'bcrypt'
 
 class User {
@@ -8,24 +7,11 @@ class User {
     }
 
     insert = () => {
-        const conn = connect();
 
         const saltRounds = 10;
 
         bcrypt.genSalt(saltRounds).then(salt => {
             bcrypt.hash(this.password, salt).then(hash => {
-                conn.query("INSERT INTO users (name, password) VALUES (?,?)", [this.name, hash], (err, result) => {
-                    if (err) {
-                        console.log(err);
-                        return "Error: insert";
-                    }
-
-                    if (result.insertId > 0) {
-                        return true;
-                    }
-
-                    return false;
-                })
             })
         })
 
@@ -33,21 +19,6 @@ class User {
     }
 
     login = (inputName, inputPassword) => {
-        const conn = connect();
 
-        conn.query("SELECT * FROM users WHERE name = ?", [inputName], (err, result) => {
-            if (err) {
-                console.log("Error: login");
-                return;
-            }
-
-            if (result.length > 0) {
-               return bcrypt.compareSync(inputPassword, result[0].password);
-            }
-
-            return "Error: failed login";
-
-
-        })
     }
 }
