ced98aab9bc5805ddc23ba8741afa3ea17185547
Raul Remesal van Merode <raul.remesal@gmail.com>
2023-09-14 14:59:28 +0200

    added switch so answer can be converted to int

--- server/models/Csv.js
+++ server/models/Csv.js
@@ -26,11 +26,24 @@ class Csv {
         setToUpdate.questions = [];
         
         dataArray.map((question) => {
-            setToUpdate.rightAnswer = question.rightAnswer;
+            const rightAnswer = question.rightAnswer.toLowerCase();
+            let answer = 0;
+            switch(rightAnswer) {
+                case 'a':
+                    answer = 0;
+                    break;
+                case 'b':
+                    answer = 1;
+                    break;
+                case 'c':
+                    answer = 2
+                    break;
+                default: null;
+            }
             setToUpdate.questions.push({
                 question: question.question,
                 answers: Array(question.A, question.B, question.C),
-                rightAnswer: question.rightAnswer
+                rightAnswer: answer,
             });
             
         });
