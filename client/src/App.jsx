e4ab48fe9b79d263e8ef1776a0cd2114ea9e9fc0
Raul Remesal van Merode <raul.remesal@gmail.com>
2023-09-14 14:59:47 +0200

    added new routes for home and question page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import MySet from './pages/MySet'
 import FillSet from './pages/FillSet'
+import Home from './pages/Home'
+import Play from './pages/Play'
 
 const App = () => {
   return (
@@ -9,6 +11,9 @@ const App = () => {
       <Routes>
         <Route path='/mysets' element={<MySet/>}/>
         <Route path='/sets/:id' element={<FillSet/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/play/:setId/:userId' element={<Play/>}/>
       </Routes>
     </Router>
   )
