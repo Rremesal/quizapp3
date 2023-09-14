5be44ad119de4b750bc558c320cc4df457ec15f9
Raul Remesal van Merode <raul.remesal@gmail.com>
2023-09-14 08:05:18 +0200

    added route for new FillSet page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import MySet from './pages/MySet'
+import FillSet from './pages/FillSet'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/mysets' element={<MySet/>}/>
+        <Route path='/sets/:id' element={<FillSet/>}/>
       </Routes>
     </Router>
   )
