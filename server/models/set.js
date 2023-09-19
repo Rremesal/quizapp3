d0b2c82af6881fda87420269c99695d82d44c9e5
Raul Remesal van Merode <raul.remesal@gmail.com>
2023-09-19 14:37:41 +0200

    changed path to generate files in

--- server/models/set.js
+++ server/models/set.js
@@ -5,7 +5,7 @@ class Set {
         this.name = name;
     }
 
-    insert = async (userId) => {
+    insert = () => {
         let defaultObject = {
             id: 0,
             setName: this.name,
@@ -17,14 +17,13 @@ class Set {
                     myAnswer: "",
                 },
             ],
-            userId: userId,
         }
 
         if(!fs.existsSync('./sets')) {
-           const path = fs.mkdirSync(`./sets/`, {recursive: true});
-           fs.writeFileSync(`${path}/${userId}.json`, JSON.stringify([defaultObject], null, 4));
+           fs.mkdirSync(`./sets/`, {recursive: true});
+           fs.writeFileSync(`sets/sets.json`, JSON.stringify([defaultObject], null, 4));
         } else {
-            const content = fs.readFileSync(`sets/${userId}.json`);
+            const content = fs.readFileSync(`sets/sets.json`);
             let jsonObj = JSON.parse(content);
             if(jsonObj.length > 0) {
                 let lastInsertId = jsonObj[jsonObj.length - 1].id;
@@ -32,21 +31,9 @@ class Set {
                 defaultObject.id = lastInsertId;
             }
             jsonObj.push(defaultObject);
-            fs.writeFileSync(`sets/${userId}.json`, JSON.stringify(jsonObj, null, 4));
+            fs.writeFileSync(`sets/sets.json`, JSON.stringify(jsonObj, null, 4));
         }
     }
-
-    // static withId = async (id) => {
-        
-    // }
-
-    // static withUserId = async (userId) => {
-        
-    // }
-
-    // static deleteWithId = async (id) => {
-    
-    // }
 
     all = () => {
 
